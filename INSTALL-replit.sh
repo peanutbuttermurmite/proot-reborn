48521921dd5e8beb7ddd19ff3d7fb15c97d5304c
peanutbuttermurmite <73216267+peanutbuttermurmite@users.noreply.github.com>
2021-10-03 09:17:01 +0100

    Update INSTALL-replit.sh

--- INSTALL-replit.sh
+++ INSTALL-replit.sh
@@ -2,7 +2,7 @@
 echo "-----> Installing proot..."
 install-pkg proot 
 echo "-----> Cleaning up filesystem"
-rm -r /home/runner/ubuntu-proot
+rm -rf /home/runner/ubuntu-proot
 mkdir /home/runner/ubuntu-proot
 cp -r assets/Ubuntu /home/runner/ubuntu-proot
 proot -S /home/runner/ubuntu-proot/Ubuntu
